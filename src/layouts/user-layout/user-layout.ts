aa77e0cabdc35405d6ddabea0d6c85f0b223575c
MomoYasser0 <myasoo28@gmail.com>
2026-06-26 19:17:59 +0300

    add UserLayout component with header, footer, and router outlet support

--- src/layouts/user-layout/user-layout.ts
+++ src/layouts/user-layout/user-layout.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLinkActive, RouterOutlet } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { PublicHeader } from '../public-layout/public-header/public-header';
 import { PublicFooter } from '../public-layout/public-footer/public-footer';
 
 @Component({
   selector: 'app-user-layout',
-  imports: [RouterLinkActive, RouterOutlet, PublicHeader, PublicFooter],
+  imports: [RouterOutlet, PublicHeader, PublicFooter],
   templateUrl: './user-layout.html',
   styles: ``,
 })
