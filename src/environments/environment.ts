cfcedb4a32639ac53b81a87b6965d3efd77fa726
Mohamed ElSefy <mohamedelsefy2003@gmail.com>
2026-06-29 14:09:41 +0300

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:44397/api/v1',
+  apiBaseUrl: 'https://localhost:7275/api/v1',
   googleClientId: '818109149867-jlbj83dcs95rknac2g38asnefamefj5o.apps.googleusercontent.com',
   agoraAppId: '',
   hubBaseUrl: 'https://10.189.240.81:7275',
   // apiBaseUrl: 'https://192.168.1.5:7275/api/v1'
-  apiBaseUrl: 'https://10.189.240.81:7275/api/v1'
+  // apiBaseUrl: 'https://10.189.240.81:7275/api/v1'
 
 };
