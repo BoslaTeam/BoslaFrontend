8ddc574c58ac9bd5846c04f4929d035718563837
osama <oe2044960@gmail.com>
2026-06-29 08:56:32 +0300

    edit ui

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // apiBaseUrl: 'https://localhost:7275/api/v1',
+  apiBaseUrl: 'https://localhost:44397/api/v1',
   googleClientId: '818109149867-jlbj83dcs95rknac2g38asnefamefj5o.apps.googleusercontent.com',
   agoraAppId: '',
-  apiBaseUrl: 'http://192.168.1.5:5250/api/v1',
 };
