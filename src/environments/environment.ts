203ef550982bf8be862650cdec0e6c2920918c81
mohamed elsefy <mohamedelsefy2003@gmail.com>
2026-07-29 08:38:31 +0300

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,8 @@
 export const environment = {
-  production: false,
-  apiBaseUrl: 'https://localhost:7275/api/v1',
+  production: true,
+  apiBaseUrl: 'https://boslaplatform.runasp.net/api/v1',
   googleClientId: '818109149867-jlbj83dcs95rknac2g38asnefamefj5o.apps.googleusercontent.com',
-  agoraAppId: '',
-  hubBaseUrl: 'https://localhost:7275',
+  agoraAppId: 'efbff60f1b8c4efeb09ebb53bdbefaa3',
+  hubBaseUrl: 'https://boslaplatform.runasp.net',
   stripePublishableKey: 'pk_test_51TkOLj05d5HmC2JPeXjUoGecsh3nv5vqPOqbCK5cDPcyM5VEzuoOxHfjAenQ7iKJiBhQsXH3RrwouMnfJ9wkWK4700ia1UKSfl',
-  // apiBaseUrl: 'https://192.168.1.2:7275/api/v1'
 };
